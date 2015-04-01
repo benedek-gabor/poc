b51a7fc91a6339e7800a85fae314c04a71a5c781
Gabor Benedek <gabor.benedek@sap.com>
2015-04-01 22:57:38 +0200

    Update mapping

--- selenium-bdd/src/test/java/com/digs/FirefoxTestCase.java
+++ selenium-bdd/src/test/java/com/digs/FirefoxTestCase.java
@@ -1,8 +1,11 @@
 package com.digs;
 
 import org.jbehave.core.annotations.Given;
+import org.jbehave.core.annotations.Then;
+import org.jbehave.core.annotations.When;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -10,18 +13,30 @@ import org.testng.annotations.Test;
 public class FirefoxTestCase {
 	
 	private WebDriver driver;
-	private String url;
 	
-	@Given(value = "")
-	public void setUp(){
-		driver = new FirefoxDriver();
-		url = "https://www.caplin.com/";
+	@Given("a &browser web driver")
+	public void setUp(String browser){
+		if(browser == "Firefox"){
+			driver = new FirefoxDriver();
+		} else{
+			throw new IllegalArgumentException("Wrong browser type.");
+		}
 	}
 	
-	public void testFirefox(){
+	@When(value = "I open this &url")
+	public void testFirefox(String url){
 		driver.get(url);
 	}
 	
+	@Then("the page title should contains: &(Caplin Systems)")
+	public void verifyPageTitle(String expTitle) {
+		String actualPageTitle = driver.getTitle();
+		String expPageTitle = expTitle;
+		
+		Assert.assertTrue(actualPageTitle.contains(expPageTitle));
+	}
+
+	@Then("Close the browser")
 	public void tearDown(){
 		driver.quit();
 	}
