cf0bbfefe1dd83145db0638dccc4326d874526ac
Gabor Benedek <gabor.benedek@sap.com>
2015-04-03 22:37:24 +0200

    Commit conflict

--- BDD_/src/test/java/steps/AbstractCountingStep.java
+++ BDD_/src/test/java/steps/AbstractCountingStep.java
@@ -5,6 +5,8 @@ import org.jbehave.core.annotations.Given;
  * Created by 212427754 on 2015.01.06..
  */
 public abstract class AbstractCountingStep {
+	
+	private int i = 10;
 
     public AbstractCountingStep(){
     }
