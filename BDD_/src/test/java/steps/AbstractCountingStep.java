0b104e4268ad34821813039fde1ac1cf2b4cd5c1
uflik <jozsak20@gmai.com>
2015-04-03 21:46:14 +0200

    Pom refactor.

--- BDD_/src/test/java/steps/AbstractCountingStep.java
+++ BDD_/src/test/java/steps/AbstractCountingStep.java
@@ -6,7 +6,6 @@ import org.jbehave.core.annotations.When;
  * Created by 212427754 on 2015.01.06..
  */
 public abstract class AbstractCountingStep {
-    protected static final org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(AbstractCountingStep.class.getName());
 
     public AbstractCountingStep(){
     }
